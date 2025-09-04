698e141ec837fad19386449d311e643037092923
Kachigalla Vaishnavi <kachigallavaishnavi@gmail.com>
2025-09-04 18:17:41 +0530

    Delete WPPass1(1.c).py

--- WPPass1(1.c).py
+++
@@ -1,9 +0,0 @@
-print("list of alphabets=")
-my_list=[(chr(i)*j) for i in range(97,123) for j in range(1,27)]
-        
-print(my_list)
-
-# print("enter names")
-# for i in range(1,11):
-#     print('name',i,input())
-
