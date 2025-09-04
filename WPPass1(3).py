cf251a668f16683b60b75fde135f6890b4afb955
Kachigalla Vaishnavi <kachigallavaishnavi@gmail.com>
2025-09-04 18:18:17 +0530

    Delete WPPass1(3).py

--- WPPass1(3).py
+++
@@ -1,5 +0,0 @@
-feet=int(input("enter length in feet:"))
-# choice=int(input("enter your choice:"))
-list=[("inches=",feet*12),("yards=",feet*0.3333),("kilometer=",feet*0.0003048),("meter=",feet*0.3048)]
-print(list)
-
